3307a380167e9f480148c3128cc8d87985799f28
Chandra Gupta Roy <65470899+chandraguptaroy@users.noreply.github.com>
2021-07-10 12:36:44 +0530

    /2

--- templates/cgrnav.js
+++ templates/cgrnav.js
@@ -6,7 +6,7 @@ document.write('<header>\
         <i class="fas fa-bars"></i>\
     </label>\
     <ul>\
-        <li><a class="" href="/blog/blog.html">Blog</a></li>\
+        <li><a class="" href="/blog/">Blog</a></li>\
         <li><a class="" href="/main/work.html">Work</a></li>\
         <li><a class="" href="/main/about.html">About</a></li>\
     </ul>\
