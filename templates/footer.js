bb0ee4740aeafd6820c14555ade3027466b7b0a9
Chandra Gupta Roy <65470899+chandraguptaroy@users.noreply.github.com>
2021-07-14 12:12:14 +0530

    footer complete

---
+++ templates/footer.js
@@ -0,0 +1,95 @@
+document.write('<footer>\
+<div class="fot-nav2">\
+            <ul>\
+                <li><a class="" href="/">Home</a></li>\
+                <li><a class="" href="/blog/">Blog</a></li>\
+                <li><a class="" href="/main/work.html">Work</a></li>\
+                <li><a class="" href="/main/about.html">About</a></li>\
+            </ul>\
+</div>\
+<div class="fot-wrapper">\
+    <section class="fot-cols">\
+        <div class="fot-col">\
+            <div class="fot-col-top">\
+                <div class="fot-content">\
+                    <div class="fot-img">\
+                        <img style="width: 25%;border-radius: 50%;" src="../images/main/eduv.jpg">\
+                    </div>\
+                    <div class="fot-details">\
+                        <h2>Eduv</h2>\
+                        <p><i>Educational Videos</i></p>\
+                    </div>\
+                    <div class="fot-social">\
+                        <a href=""><i class="fab fa-youtube"></i></a>\
+                        <a href=""><i class="fab fa-instagram"></i></a>\
+                        <a href=""><i class="fab fa-linkedin"></i></a>\
+                        <a href=""><i class="fab fa-twitter"></i></a>\
+                        <a href=""><i class="fab fa-facebook-f"></i></a>\
+                        <a href=""><i class="fab fa-spotify"></i></a>\
+                        <a href=""><i class="fab fa-tumblr"></i></a>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+        <div class="fot-col">\
+            <div class="fot-col-top">\
+                <div class="fot-content">\
+                    <div class="fot-img">\
+                        <img style="width: 25%;border-radius: 50%;" src="../images/main/chandu.png">\
+                    </div>\
+                    <div class="fot-details">\
+                        <h2>CGR</h2>\
+                        <p><i>Vlogs | Tutorials</i></p>\
+                    </div>\
+                    <div class="fot-social">\
+                        <a href=""><i class="fab fa-youtube"></i></a>\
+                        <a href=""><i class="fab fa-instagram"></i></a>\
+                        <a href=""><i class="fab fa-linkedin"></i></a>\
+                        <a href=""><i class="fab fa-twitter"></i></a>\
+                        <a href=""><i class="fab fa-facebook-f"></i></a>\
+                        <a href=""><i class="fab fa-spotify"></i></a>\
+                        <a href=""><i class="fab fa-tumblr"></i></a>\
+                       <!-- <a href=""><i><img class="fot-duolingo" src="/images/icons/duolingo.png"></i></a>-->\
+                        <a href=""class="fot-duo iconify duolingo" data-icon="simple-icons:duolingo"></a>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+        <div class="fot-col">\
+            <div class="fot-col-top">\
+                <div class="fot-content">\
+                    <div class="fot-img">\
+                        <img style="width: 25%;border-radius: 50%;" src="/images/main/why it.jpg">\
+                    </div>\
+                    <div class="fot-details">\
+                        <h2>Why it</h2>\
+                        <p><i>Exploration of Curiosity</i></p>\
+                    </div>\
+                    <div class="fot-social">\
+                        <a href=""><i class="fab fa-youtube"></i></a>\
+                        <a href=""><i class="fab fa-instagram"></i></a>\
+                        <a href=""><i class="fab fa-linkedin"></i></a>\
+                        <a href=""><i class="fab fa-twitter"></i></a>\
+                        <a href=""><i class="fab fa-facebook-f"></i></a>\
+                        <a href=""><i class="fab fa-spotify"></i></a>\
+                        <a href=""><i class="fab fa-tumblr"></i></a>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </section>\
+</div>\
+    <div class="fot-second">\
+        <hr color=white width="10%" size="1px" />\
+        <p style="text-align: center;color: white;">Chandra Gupta Roy | @chandraguptaroy</p>\
+    </div>\
+    <div class="fot-support">\
+        <h3 style="color: rgb(0, 0, 0);">Support:</h3>\
+        <div class="fot-support-icon">\
+                <a href=""><img class="icon" src="/images/payments icons/gpay.png"></a>\
+                <a href=""><img class="icon paypal"  src="/images/payments icons/paypal-logo.png"></a>\
+                <a href=""><img  class="icon " src="/images/payments icons/paytm-226448.png"></a>\
+        </div>\
+    </div>\
+</footer>\
+    ');
